51252bb71149091e7efa65d5a4f54d127142cf37
Sankha Cooray <bsc2fast@hotmail.com>
2022-08-17 10:34:33 +0800

    debug update

--- random/service-worker.js
+++ random/service-worker.js
@@ -174,7 +174,7 @@ define("./service-worker.js",['./workbox-176fe0b1'], (function (workbox) { 'use 
     "revision": "0c9930bb47d010604e9d06414cbeefe4"
   }, {
     "url": "/random/js/app.js",
-    "revision": "bcdb0de1a8822a29688a7f5db790dd30"
+    "revision": "f012a716ced9476ec83bbbb7ba29dcd1"
   }, {
     "url": "/random/js/chunk-vendors.js",
     "revision": "63854a2806d3bbbb875ebd9a9fed8ac3"
