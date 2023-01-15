4a8e7f960d421198fc17a3cea8b72743637f7daf
Sankha Cooray <bsc2fast@hotmail.com>
2023-01-15 17:42:52 +0800

    few updates - test

--- random/service-worker.js
+++ random/service-worker.js
@@ -174,7 +174,7 @@ define("./service-worker.js",['./workbox-176fe0b1'], (function (workbox) { 'use 
     "revision": "0c9930bb47d010604e9d06414cbeefe4"
   }, {
     "url": "/random/js/app.js",
-    "revision": "5ab93ef74c8187d34578803e8bfb4c81"
+    "revision": "52757b27629be2d5fdd3dd9b8ecd82ad"
   }, {
     "url": "/random/js/chunk-vendors.js",
     "revision": "63854a2806d3bbbb875ebd9a9fed8ac3"
